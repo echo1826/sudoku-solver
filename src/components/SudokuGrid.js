c74db27dd1bc14f08780e775a07de9e89a9d1e4f
Ethan Cho <ethan.cho1826@gmail.com>
2021-12-24 19:24:05 -0600

    solve button added, need to add functionality to it and css the inputs to look like grid

--- src/components/SudokuGrid.js
+++ src/components/SudokuGrid.js
@@ -4,7 +4,10 @@ export default function SudokuGrid() {
     
     return (
         <div>
-            {[...Array(81)].map((x, i) =><input type='number' min='1' max='9' key={i}></input>)}
+            <div className = "sudokuBoard">
+                {[...Array(81)].map((x, i) =><input type='number' min='1' max='9' key={i}></input>)}
+            </div>
+            <button>Solve</button>
         </div>
     );
 }
